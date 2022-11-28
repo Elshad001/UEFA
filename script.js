1e45ed0a1b7bc4e033087694f1564a6b1161551c
Elshad001 <elshad.ismayilovv@mail.ru>
2022-11-28 11:02:50 +0400

    test

---
+++ script.js
@@ -0,0 +1,13 @@
+function menubar()
+{
+    var allsections=document.querySelector("#allsections");
+    allsections.style.visibility="visible";
+    
+}
+
+
+function backall()
+{
+    
+    allsections.style.visibility="hidden";
+}
